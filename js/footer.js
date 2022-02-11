55225a7ef9be96247dd1c207b52a6deb7ad6a761
renkaigis <renkaigis@163.com>
2022-02-11 17:04:36 +0800

    .

--- js/footer.js
+++ js/footer.js
@@ -1,26 +1,59 @@
-document.writeln("		<footer>");
-document.writeln("			<div class=\'zerogrid wrap-footer\'>");
-document.writeln("				<div class=\'row\'>");
-document.writeln("					<div class=\'bottom-social\'>");
-document.writeln("						<a href=\'http://renkaigis.com\' target=\'_blank\'><i class=\'fa fa-rss\'");
-document.writeln("								style=\'color: purple;\'></i></a>");
-document.writeln("						<a href=\'http://blog.renkaigis.com\' target=\'_blank\'><i class=\'fa fa-coffee\'");
-document.writeln("								style=\'color: purple;\'></i></a>");
-document.writeln("						<a href=\'https://github.com/renkaigis\' target=\'_blank\'><i class=\'fa fa-github-alt\'");
-document.writeln("								style=\'color: purple;\'></i></a>");
-document.writeln("						<a href=\'http://resume.renkaigis.com\' target=\'_blank\'><i class=\'fa fa-heartbeat\'");
-document.writeln("								style=\'color: purple;\'></i></a>");
-document.writeln("						<a href=\'http://photo.renkaigis.com\' target=\'_blank\'><i class=\'fa fa-camera-retro\'");
-document.writeln("								style=\'color: purple;\'></i></a>");
-document.writeln("					</div>");
+document.writeln("	<footer>");
+document.writeln("		<div class=\'zerogrid wrap-footer\'>");
+document.writeln("			<div class=\'row\'>");
+document.writeln("				<div class=\'bottom-social\'>");
+document.writeln("					<a href=\'http://renkaigis.com\' target=\'_blank\'><i class=\'fa fa-rss\' style=\'color: purple;\'></i></a>");
+document.writeln("					<a href=\'http://blog.renkaigis.com\' target=\'_blank\'><i class=\'fa fa-coffee\'");
+document.writeln("							style=\'color: purple;\'></i></a>");
+document.writeln("					<a href=\'https://github.com/renkaigis\' target=\'_blank\'><i class=\'fa fa-github-alt\'");
+document.writeln("							style=\'color: purple;\'></i></a>");
+document.writeln("					<a href=\'http://resume.renkaigis.com\' target=\'_blank\'><i class=\'fa fa-heartbeat\'");
+document.writeln("							style=\'color: purple;\'></i></a>");
+document.writeln("					<a href=\'http://photo.renkaigis.com\' target=\'_blank\'><i class=\'fa fa-camera-retro\'");
+document.writeln("							style=\'color: purple;\'></i></a>");
 document.writeln("				</div>");
-document.writeln("				<div class=\'copyright\'>");
-document.writeln("					<a href=\'https://beian.miit.gov.cn/\' target=\'_blank\'");
-document.writeln("						style=\'color: #C07AB8;font-size: 80%;font-weight: normal;\'>陕ICP备17015858号</a>");
-document.writeln("				</div>");
-document.writeln("				<div class=\'copyright\'>");
-document.writeln("					Copyright &copy; 2021. Kai All rights reserved.<a target=\'_blank\'");
-// document.writeln("					Copyright &copy; 2021. Kai & Ru All rights reserved.<a target=\'_blank\'");
-document.writeln("						href=\'http://renkaigis.com\'></a>");
-document.writeln("				</div>");
-document.writeln("		</footer>");
+document.writeln("			</div>");
+document.writeln("");
+document.writeln("			<div class=\'copyright\'>");
+document.writeln("				<a target=\'_blank\' href=\'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=61909002000104\'");
+document.writeln("					style=\'color: #C07AB8;font-size: 80%;font-weight: normal;display:inline-block;height:20px;line-height:20px;\'><img");
+document.writeln("						src=\'images/beian.png\' style=\'float:left;\' />");
+document.writeln("					<p style=\'float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;\'>陕公网安备");
+document.writeln("						61909002000104号</p>");
+document.writeln("				</a>");
+document.writeln("			</div>");
+document.writeln("");
+document.writeln("			<div class=\'copyright\'>");
+document.writeln("				Copyright &copy; 2022. Kai All rights reserved.<a target=\'_blank\' href=\'http://renkaigis.com\'></a>");
+document.writeln("			</div>");
+document.writeln("	</footer>");
+
+{/* <footer>
+<div class='zerogrid wrap-footer'>
+    <div class='row'>
+        <div class='bottom-social'>
+            <a href='http://renkaigis.com' target='_blank'><i class='fa fa-rss' style='color: purple;'></i></a>
+            <a href='http://blog.renkaigis.com' target='_blank'><i class='fa fa-coffee'
+                    style='color: purple;'></i></a>
+            <a href='https://github.com/renkaigis' target='_blank'><i class='fa fa-github-alt'
+                    style='color: purple;'></i></a>
+            <a href='http://resume.renkaigis.com' target='_blank'><i class='fa fa-heartbeat'
+                    style='color: purple;'></i></a>
+            <a href='http://photo.renkaigis.com' target='_blank'><i class='fa fa-camera-retro'
+                    style='color: purple;'></i></a>
+        </div>
+    </div>
+
+    <div class='copyright'>
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=61909002000104"
+            style="color: #C07AB8;font-size: 80%;font-weight: normal;display:inline-block;height:20px;line-height:20px;"><img
+                src="images/beian.png" style="float:left;" />
+            <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;">陕公网安备
+                61909002000104号</p>
+        </a>
+    </div>
+
+    <div class='copyright'>
+        Copyright &copy; 2022. Kai All rights reserved.<a target='_blank' href='http://renkaigis.com'></a>
+    </div>
+</footer> */}
